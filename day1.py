791c17f4a725465f2e73b48562ca671b20bd355e
joyzo14 <hostak33@gmail.com>
2021-12-01 14:42:34 +0100

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,33 @@
+import re
+
+def solve(input):
+
+    measurements = input.splitlines()
+
+    count = 0
+    for i in range(len(measurements)-1):
+        if(int(measurements[i]) < int(measurements[i+1])):
+            count +=1
+
+    print(str(count) + " measurements are larger than the previous measurement")
+
+    # 2nd part
+    listOfSums=[] #list of triples
+    for i in range(len(measurements)-2):
+        listOfSums.append(int(measurements[i]) + int(measurements[i+1]) + int(measurements[i+2]))
+
+    count = 0
+    for i in range(len(listOfSums) - 1):
+        if (int(listOfSums[i]) < int(listOfSums[i + 1])):
+            count += 1
+
+    print(str(count) + " sums are larger than the previous sum")
+
+def main():
+    with open('textfile.txt', 'r') as f:
+        fileinput = f.read()
+        solve(fileinput)
+
+
+if __name__ == '__main__':
+    main()
